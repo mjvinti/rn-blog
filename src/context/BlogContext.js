56e0a181dc5901750505b2611029a92c862029dd
Mario Vinti <mjvinti@gmail.com>
2022-11-07 12:28:23 -0500

    moving data with context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,5 +3,7 @@ import React, { createContext } from "react";
 const BlogContext = createContext();
 
 export const BlogProvider = ({ children }) => (
-  <BlogContext.Provider>{children}</BlogContext.Provider>
+  <BlogContext.Provider value={[1, 2, 3]}>{children}</BlogContext.Provider>
 );
+
+export default BlogContext;
