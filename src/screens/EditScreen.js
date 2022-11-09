481358ea7833649218a70d841c619f562abf79c1
Mario Vinti <mjvinti@gmail.com>
2022-11-09 12:34:16 -0500

    initializing state from context

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,10 +1,24 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useContext, useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
+  );
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>EditScreen - {navigation.getParam("id")}</Text>
+      <Text>Edit Title:</Text>
+      <TextInput onChangeText={setTitle} value={title} />
+      <Text>Edit content:</Text>
+      <TextInput onChangeText={setContent} value={content} />
     </View>
   );
 };
