0812f9f59fbb420f6435a32cb59f14b7acae3fc3
Mario Vinti <mjvinti@gmail.com>
2022-11-09 15:35:49 -0500

    added navigating backwards on edit blog post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -25,7 +25,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
-    callback();
+    if (callback) {
+      callback();
+    }
   };
 };
 
@@ -34,8 +36,12 @@ const deleteBlogPost = (dispatch) => {
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content) =>
+  return (id, title, content, callback) => {
     dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    if (callback) {
+      callback();
+    }
+  };
 };
 
 export const { Context, Provider } = createDataContext(
