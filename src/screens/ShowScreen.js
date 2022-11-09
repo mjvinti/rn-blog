fb18cff85a2a50a7d52433faeb3fb86c625dad4e
Mario Vinti <mjvinti@gmail.com>
2022-11-09 12:12:14 -0500

    added edit icon link

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { EvilIcons } from "@expo/vector-icons";
 
 import { Context } from "../context/BlogContext";
 
@@ -13,8 +14,19 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
