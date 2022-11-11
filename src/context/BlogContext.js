593cf6b0ec7a4157dd7ece0b3a4302aa58cb1697
Mario Vinti <mjvinti@gmail.com>
2022-11-11 14:18:55 -0500

    deleting blog post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,15 +5,6 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case "get_blogposts":
       return action.payload;
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "edit_blogpost":
@@ -35,7 +26,7 @@ const getBlogPosts = (dispatch) => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
     await jsonServer.post("/blogposts", { title, content });
-    // dispatch({ type: "add_blogpost", payload: { title, content } });
+
     if (callback) {
       callback();
     }
@@ -43,7 +34,10 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => dispatch({ type: "delete_blogpost", payload: id });
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
 };
 
 const editBlogPost = (dispatch) => {
